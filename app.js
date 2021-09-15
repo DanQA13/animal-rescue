2b5e82dd9d3475995ab6cc8f1b14f384e21eb6bd
Daniel Cornier <D.cornier1386@outlook.com>
2021-09-15 00:57:47 -0400

    day 7 push

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+
+let answer = prompt('Do you love animals?');
+
+
+  if(answer.toLowerCase() === 'yes'){
+    alert('Then you have come to the right place!');
+ 
+ } else if(answer.toLowerCase() === 'no'){
+    alert('Then you will be kindly removed');
+
+    }  
+    let favanimal = prompt ('Do you want to see one?');   
+    
+    if(favanimal.toLowerCase() === 'yes'){
+        alert('Then come down and volunteer at our shelter')
+    }
+
+    let description = prompt('Lets hear your best animal joke and we will post it to our site ');
+
+    document.write('<p>' + description + '</p>');
+
+
+    
+    
+
+
+
+     
+    
+        
+
+    
