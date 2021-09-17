c3e3e181e045a3842f550d757f2f181ee3761242
Daniel Cornier <D.cornier1386@outlook.com>
2021-09-16 23:36:08 -0400

    lsast poush for 102

--- app.js
+++ app.js
@@ -21,15 +21,7 @@ let answer = prompt('Do you love animals?');
     if(description.toLowerCase() === '24/7'){
       alert('Correct check the clock below we are here 24/7')
   }
-        
-
-
-  
-
- 
-
-
-    function showTime(){
+        function showTime(){
       var date = new Date();
       var h = date.getHours(); // 0 - 23
       var m = date.getMinutes(); // 0 - 59
@@ -56,7 +48,18 @@ let answer = prompt('Do you love animals?');
       setTimeout(showTime, 1000);
       
   }
-  
   showTime();
 
+  askforimages();
+
  
+  function askforimages() {
+    let howmany = prompt('How many animals are you looking to adopt today');
+    let images = document.querySelector('#images');
+        if (1 + 1 === 2) {
+          document.getElementById('submit').addEventListener('click', generate);
+        }
+        for(let i = howmany; i > 0; i--){
+          document.write("<img src= 'https://image.shutterstock.com/shutterstock/photos/1826554397/display_1500/stock-photo-young-nice-looking-couple-wants-to-adopt-beautiful-dog-at-animal-shelter-1826554397.jpg' style='width: 400px; height: 350px;' />");
+        }
+      }
